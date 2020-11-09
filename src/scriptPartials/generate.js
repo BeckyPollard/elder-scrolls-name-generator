32261da47229dcb8a4acf404ef319cf9bb4b878c
BeckyPollard <hello@beckypollard.com>
2020-11-09 17:02:20 -0500

    Specify name generate by gender, add m&f array

--- src/scriptPartials/generate.js
+++ src/scriptPartials/generate.js
@@ -1,14 +1,28 @@
 //example of partialed scripts
-import { femaleDunmerNames, maleDunmerNames, familyDunmerNames } from './names';
+import { femaleDunmerNames, maleDunmerNames, dunmerNames, familyDunmerNames } from './names';
 
 export const generateNames = () => {
   document.getElementById('names').innerHTML = '';
   let generatedNames = [];
 
-  for (let i = 0; i < 10; i++) {
-    let randFirstName = femaleDunmerNames[Math.floor(Math.random() * femaleDunmerNames.length)];
-    let randLastName = familyDunmerNames[Math.floor(Math.random() * familyDunmerNames.length)];
-    generatedNames.push(`${randFirstName} ${randLastName}`);
+  if (document.getElementById("options-gender-male").checked) {
+    for (let i = 0; i < 10; i++) {
+      let randFirstName = maleDunmerNames[Math.floor(Math.random() * maleDunmerNames.length)];
+      let randLastName = familyDunmerNames[Math.floor(Math.random() * familyDunmerNames.length)];
+      generatedNames.push(`${randFirstName} ${randLastName}`);
+    }
+  } else if (document.getElementById("options-gender-female").checked) {
+    for (let i = 0; i < 10; i++) {
+      let randFirstName = femaleDunmerNames[Math.floor(Math.random() * femaleDunmerNames.length)];
+      let randLastName = familyDunmerNames[Math.floor(Math.random() * familyDunmerNames.length)];
+      generatedNames.push(`${randFirstName} ${randLastName}`);
+    }
+  } else {
+    for (let i = 0; i < 10; i++) {
+      let randFirstName = dunmerNames[Math.floor(Math.random() * dunmerNames.length)];
+      let randLastName = familyDunmerNames[Math.floor(Math.random() * familyDunmerNames.length)];
+      generatedNames.push(`${randFirstName} ${randLastName}`);
+    }
   }
 
   for (let i = 0; i < generatedNames.length; i++) {
--- src/scriptPartials/names.js
+++ src/scriptPartials/names.js
@@ -2560,6 +2560,8 @@ export const maleDunmerNames = [
   'Xiomara',
 ];
 
+export const dunmerNames = [].concat(femaleDunmerNames, maleDunmerNames);
+
 export const familyDunmerNames = [
   'Adram',
   'Adryn',
