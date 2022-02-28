0d24e6c7ba17ce70c4c967cde94a7655c4e661b6
BeckyPollard <hello@beckypollard.com>
2022-02-27 19:41:44 -0500

    Add scraping for mer races names

---
+++ src/scripts/names/names.d.ts
@@ -0,0 +1,16 @@
+export const altmerFemaleNames = [string];
+export const altmerMaleNames = [string];
+export const altmerAllNames = [string];
+
+export const bosmerFemaleNames = [string];
+export const bosmerMaleNames = [string];
+export const bosmerAllNames = [string];
+
+export const dunmerFemaleNames = [string];
+export const dunmerMaleNames = [string];
+export const dunmerAllNames = [string];
+export const dunmerFamilyNames = [string];
+
+export const orsimerFemaleNames = [string];
+export const orsimerMaleNames = [string];
+export const orsimerAllNames = [string];
--- src/scripts/names/scrape.js
+++ src/scripts/names/scrape.js
@@ -4,18 +4,104 @@ const cheerio = require('cheerio'); // to parse web page
 const fs = require('fs'); // to write files with node
 
 // files to write:
-const altmerNames = fs.createWriteStream('./altmer.js');
-const argonianNames = fs.createWriteStream('./argonian.js');
-const bosmerNames = fs.createWriteStream('./bosmer.js');
-const bretonNames = fs.createWriteStream('./breton.js');
-const dunmerNames = fs.createWriteStream('./dunmer.js');
-const imperialNames = fs.createWriteStream('./imperial.js');
-const khajiitNames = fs.createWriteStream('./khajiit.js');
-const nordNames = fs.createWriteStream('./nord.js');
-const orcNames = fs.createWriteStream('./orc.js');
-const redguardNames = fs.createWriteStream('./redguard.js');
+// const altmerNames = fs.createWriteStream('./altmer.js');
+// const argonianNames = fs.createWriteStream('./argonian.js');
+// const bosmerNames = fs.createWriteStream('./bosmer.js');
+// const bretonNames = fs.createWriteStream('./breton.js');
+// const dunmerNames = fs.createWriteStream('./dunmer.js');
+// const imperialNames = fs.createWriteStream('./imperial.js');
+// const khajiitNames = fs.createWriteStream('./khajiit.js');
+// const nordNames = fs.createWriteStream('./nord.js');
+// const orcNames = fs.createWriteStream('./orc.js');
+// const redguardNames = fs.createWriteStream('./redguard.js');
+
+const names = fs.createWriteStream('./names.js');
 
 // GET NAMES
+//// ALTMER NAMES
+axios.get('https://en.uesp.net/wiki/Lore:Altmer_Names').then((res) => {
+  const $ = cheerio.load(res.data);
+
+  // GET NAMES BY GENDER
+  const namesFemale = [];
+  $('#Female_Altmer_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesFemale.push(`"${name}"`);
+      }
+    });
+  const namesMale = [];
+  $('#Male_Altmer_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesMale.push(`"${name}"`);
+      }
+    });
+
+  // remove dup names
+  const onlyUniqueNames = (val, index, test) => {
+    return test.indexOf(val) === index;
+  };
+  const uniqueFemaleNames = namesFemale.filter(onlyUniqueNames);
+  const uniqueMaleNames = namesMale.filter(onlyUniqueNames);
+
+  names.write(`export const altmerFemaleNames = [${uniqueFemaleNames}]; \n\n`);
+  names.write(`export const altmerMaleNames = [${uniqueMaleNames}]; \n\n`);
+  names.write(`export const altmerAllNames = [${uniqueMaleNames.concat(uniqueFemaleNames)}]; \n\n`);
+});
+
+//// BOSMER NAMES
+axios.get('https://en.uesp.net/wiki/Lore:Bosmer_Names').then((res) => {
+  const $ = cheerio.load(res.data);
+
+  // GET NAMES BY GENDER
+  const namesFemale = [];
+  $('#Female_Bosmer_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesFemale.push(`"${name}"`);
+      }
+    });
+  const namesMale = [];
+  $('#Male_Bosmer_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesMale.push(`"${name}"`);
+      }
+    });
+
+  // remove dup names
+  const onlyUniqueNames = (val, index, test) => {
+    return test.indexOf(val) === index;
+  };
+  const uniqueFemaleNames = namesFemale.filter(onlyUniqueNames);
+  const uniqueMaleNames = namesMale.filter(onlyUniqueNames);
+
+  names.write(`export const bosmerFemaleNames = [${uniqueFemaleNames}]; \n\n`);
+  names.write(`export const bosmerMaleNames = [${uniqueMaleNames}]; \n\n`);
+  names.write(`export const bosmerAllNames = [${uniqueMaleNames.concat(uniqueFemaleNames)}]; \n\n`);
+});
+
 //// DUNMER NAMES
 axios.get('https://en.uesp.net/wiki/Lore:Dunmer_Names').then((res) => {
   const $ = cheerio.load(res.data);
@@ -82,8 +168,50 @@ axios.get('https://en.uesp.net/wiki/Lore:Dunmer_Names').then((res) => {
   const uniqueMaleNames = namesMale.filter(onlyUniqueNames);
   const uniqueFamilyNames = namesFamily.filter(onlyUniqueNames);
 
-  dunmerNames.write(`export const femaleNames = [${uniqueFemaleNames}]; \n\n`);
-  dunmerNames.write(`export const maleNames = [${uniqueMaleNames}]; \n\n`);
-  dunmerNames.write(`export const allNames = [${uniqueMaleNames.concat(uniqueFemaleNames)}]; \n\n`);
-  dunmerNames.write(`export const familyNames = [${uniqueFamilyNames}]; \n`);
+  names.write(`export const dunmerFemaleNames = [${uniqueFemaleNames}]; \n\n`);
+  names.write(`export const dunmerMaleNames = [${uniqueMaleNames}]; \n\n`);
+  names.write(`export const dunmerAllNames = [${uniqueMaleNames.concat(uniqueFemaleNames)}]; \n\n`);
+  names.write(`export const dunmerFamilyNames = [${uniqueFamilyNames}]; \n`);
+});
+
+//// ORSIMER NAMES
+axios.get('https://en.uesp.net/wiki/Lore:Orc_Names').then((res) => {
+  const $ = cheerio.load(res.data);
+
+  // GET NAMES BY GENDER
+  const namesFemale = [];
+  $('#Female_Orc_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesFemale.push(`"${name}"`);
+      }
+    });
+  const namesMale = [];
+  $('#Male_Orc_Names')
+    .parent()
+    .nextUntil('h2')
+    .children('a')
+    .each((index, element) => {
+      const name = $(element).text();
+      // simple do not push numbers check
+      if(name.length > 1) {
+        namesMale.push(`"${name}"`);
+      }
+    });
+
+  // remove dup names
+  const onlyUniqueNames = (val, index, test) => {
+    return test.indexOf(val) === index;
+  };
+  const uniqueFemaleNames = namesFemale.filter(onlyUniqueNames);
+  const uniqueMaleNames = namesMale.filter(onlyUniqueNames);
+
+  names.write(`export const orcFemaleNames = [${uniqueFemaleNames}]; \n\n`);
+  names.write(`export const orcMaleNames = [${uniqueMaleNames}]; \n\n`);
+  names.write(`export const orcAllNames = [${uniqueMaleNames.concat(uniqueFemaleNames)}]; \n\n`);
 });
